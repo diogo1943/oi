33b8e9cd256deafecd918da1929913460db0aeaa
diogo1943 <51096436+diogo1943@users.noreply.github.com>
2020-08-07 11:28:55 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,131 @@
+render();
+
+var close = document.getElementById("close");
+close.addEventListener("click", close_proj);
+
+document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        var check_zoom = document.getElementById('zoomed');
+
+        if (check_zoom != null) {
+            console.log('ckajdshkah');
+            zoom_out();
+        } else {
+            close_proj();
+        }
+    }
+};
+
+
+function render() {
+
+    var i
+
+    for (var i = projs_new.length; i--;) {
+        var today = new Date();
+        var proj_date = new Date((projs_new[i])['date']);
+
+        if (today > proj_date) {
+            var p = document.createElement('p');
+            var a = document.createElement('a');
+            a.classList.add("item_links");
+            console.log((projs_new[i])['title_home']);
+            a.appendChild(document.createTextNode((projs_new[i])['title_home']));
+            var img = document.createElement("IMG");
+            img.setAttribute("src", "img/work/" + (projs_new[i])['thumbnail']);
+            p.appendChild(a);
+            a.appendChild(img);
+            a.setAttribute("id", (projs_new[i])['index']);
+            document.getElementById("list").appendChild(p);
+            a.addEventListener("click", openproj);
+
+            var proj_date_new = new Date((projs_new[i])['date']);
+            proj_date_new.setDate(proj_date_new.getDate() + 15);
+
+            if (today < proj_date_new) {
+                var sup = document.createElement('sup');
+                var newtext = document.createTextNode(' NEW!')
+                sup.appendChild(newtext);
+                sup.classList.add('new')
+                p.appendChild(sup)
+            }
+        }
+    }
+}
+
+function openproj() {
+
+    var i = event.srcElement.id;
+    //console.log(i);
+    //console.log((projs[i])[1]);
+
+    document.getElementById("list").classList.add("hidden");
+    document.getElementById("proj").classList.remove("hidden");
+
+    //document.getElementById("work_images").src = "contents/" + (projs[i])[2];
+
+    document.getElementById("title").innerHTML = (projs_new[i])['title_full'];
+    document.getElementById("service").innerHTML = (projs_new[i])['service'];
+    document.getElementById("work_text").innerHTML = (projs_new[i])['text'];
+
+    /* experimentar carregar os conteudos diretamente daqui */
+
+    document.getElementById("collabs").innerHTML = (projs_new[i])['collaboration'];
+
+    var images = (projs_new[i])['images']
+
+    for (v = 0; v < images.length; v++) {
+        console.log('oi');
+        var img = document.createElement("IMG");
+        var src = images[v];
+        //console.log(src);
+        img.setAttribute("id", v);
+        img.setAttribute("src", "img/work/" + src);
+        img.classList.add('galeria_img');
+        img.addEventListener('click', zoom_img);
+        document.getElementById("galeria").appendChild(img);
+    }
+}
+
+function close_proj() {
+    document.getElementById("list").classList.remove("hidden");
+    document.getElementById("galeria").innerHTML = '';
+    document.getElementById("proj").classList.add("hidden");
+    window.scrollTo(0, 0);
+}
+
+function zoom_img() {
+    console.log('zoom in');
+
+    var i = event.srcElement.src;
+    var id = event.srcElement.id;
+    //console.log(i);
+
+    var dark_back = document.createElement('div');
+    dark_back.setAttribute('id', 'zoomed');
+    dark_back.addEventListener('click', zoom_out);
+
+    var zoomed_img = document.createElement('img');
+    zoomed_img.setAttribute('src', i);
+    zoomed_img.setAttribute('id', id);
+
+    dark_back.appendChild(zoomed_img);
+
+    var esc = document.createElement('p');
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        esc.appendChild(document.createTextNode('Touch anywhere to close'));
+    } else {
+        esc.appendChild(document.createTextNode('x'));
+    }
+    //esc.classList.add('item_links');
+    esc.addEventListener('click', zoom_out);
+    dark_back.appendChild(esc);
+
+    document.body.appendChild(dark_back);
+
+}
+
+function zoom_out() {
+    document.body.removeChild(document.getElementById('zoomed'));
+}
---
+++ js/vars.js
@@ -0,0 +1,142 @@
+var projs_new = [
+
+    {
+        index: 0,
+        title_home: 'Just The Wind (2019)',
+        title_full: 'Just The Wind (2019)',
+        date: 'October 12, 2019 11:13:00',
+        service: 'Art Direction / Editorial Design',
+        text: "<i>Just the Wind</i> é uma revista sobre contrastes, sobre a dualidade dos extremos. O título provém de uma das cartas homónimas do jogo Magic The Gathering, que alude ao momento em que alguém pergunta “O que foi aquilo?” e alguém responde “Nada, foi apenas o vento”. O primeiro número – “The Wind and The Storm” – centra-se na ideia de humanidade e nos seus estados futuros até que esta atinja o (provável) cume da sua condição. Procurámos explorar as fronteiras entre a real insignificância e a insignificância do real, revelando a big picture de forma dramática, mas tendo sempre em mente que, tal como no mote da carta de Magic “Just the Wind”, “There's nothing to worry about”.",
+        collaboration: 'colab w/:<br>Tiago Nunes<br><br>',
+        thumbnail: 'just_the_wind-01.jpg',
+        images: ["just_the_wind-01.jpg", "just_the_wind-02.jpg", "just_the_wind-03.jpg", "just_the_wind-04.jpg", "just_the_wind-05.jpg", "just_the_wind-06.jpg", "just_the_wind-07.jpg", "just_the_wind-08.jpg", "just_the_wind-09.jpg", ]
+    },
+    {
+        index: 1,
+        title_home: '420@ôa (2019)',
+        title_full: '420@ôa (2019)',
+        date: 'October 12, 2019 11:13:00',
+        service: 'Art Direction',
+        text: '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.',
+        collaboration: '',
+        thumbnail: 'mike_tilt.png',
+        images: ['420atoa_logo.svg', '420atoa.jpg']
+    },
+    {
+        index: 2,
+        title_home: "As Foretold (2019)",
+        title_full: "As Foretold (2019)",
+        date: 'October 12, 2019 11:13:00',
+        service: 'Editorial Design / Web Development / Webdesign',
+        text: '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.<br><br><a class="proj_links" target="blank" href="http://2020.fbaul-dcnm.pt/evolutionary_leap/">Project Website →</a>',
+        collaboration: 'colab w/:<br>Beatriz Pinta Gama<br>Helena Barradas<br><br>',
+        thumbnail: 'foretold.jpg',
+        images: ['foretold.jpg']
+    },
+    {
+        index: 3,
+        title_home: "It Takes Several Minutes... (2019)",
+        title_full: "It Takes Several Minutes<br>For The Eyes To Adjust To The Dark (2019)",
+        date: 'October 12, 2019 11:13:00',
+        service: 'Art Direction / Web Development / Webdesign',
+        text: "<i>Just the Wind</i> é uma revista sobre contrastes, sobre a dualidade dos extremos. O título provém de uma das cartas homónimas do jogo Magic The Gathering, que alude ao momento em que alguém pergunta “O que foi aquilo?” e alguém responde “Nada, foi apenas o vento”. O primeiro número – “The Wind and The Storm” – centra-se na ideia de humanidade e nos seus estados futuros até que esta atinja o (provável) cume da sua condição. Procurámos explorar as fronteiras entre a real insignificância e a insignificância do real, revelando a big picture de forma dramática, mas tendo sempre em mente que, tal como no mote da carta de Magic “Just the Wind”, “There's nothing to worry about”.<br><br><a class='proj_links' target='blank' href='http://ittakesseveralminutes.belasartes.ulisboa.pt/'>Project Website →</a>",
+        collaboration: 'colab w/:<br>Afonso de Matos<br>Beatriz Pinta Gama<br>Inês Pinheiro<br>Nádia Alexandre<br>Vitor Serra<br><br>',
+        thumbnail: 'it_takes/it_takes.png',
+        images: ['it_takes/it_takes.png', 'it_takes/it_takes_01.jpg', 'it_takes/it_takes_site-01.png', 'it_takes/it_takes_site-02.png', 'it_takes/it_takes_site-03.png', 'it_takes/it_takes_site-04.png', 'it_takes/it_takes_site-05.png', 'it_takes/it_takes_site-06.png']
+    },
+    {
+        index: 4,
+        title_home: 'Evolutionary Leap® (2020)',
+        title_full: 'Evolutionary Leap® (2020)',
+        date: 'October 12, 2019 11:13:00',
+        service: 'Editorial Design / Web Development / Webdesign',
+        text: '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.<br><br><a class="proj_links" target="blank" href="http://2020.fbaul-dcnm.pt/evolutionary_leap/">Project Website →</a>',
+        collaboration: '',
+        thumbnail: "leap/leap.gif",
+        images: ['leap/leap-logo.svg', 'leap/leap.gif', 'leap/leap-01.jpg', 'leap/leap-02.jpg', 'leap/leap-03.jpg', 'leap/leap-04.jpg', 'leap/leap-05.jpg', 'leap/leap-06.jpg', 'leap/leap-07.jpg', 'leap/leap_site-01.png', 'leap/leap_site-02.png', 'leap/leap_site-03.png']
+    },
+    {
+        index: 5,
+        title_home: 'Cinenova (2020)',
+        title_full: 'Cinenova (2020)',
+        date: 'October 12, 2019 11:13:00',
+        service: 'Art Direction / Brand Identity / Web Development / Webdesign',
+        text: '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.<br><br><a class="proj_links" target="blank" href="https://cinenova.fcsh.unl.pt/">Project Website →</a>',
+        collaboration: 'colab w/:<br>Tiago Nunes<br><br>',
+        thumbnail: 'cinenova/cinenova.png',
+        images: ['cinenova/cine_logo.svg', 'cinenova/cinenova.png', 'cinenova/poster-01.png', 'cinenova/cine_site-01.png', 'cinenova/cine_site-02.png', 'cinenova/cine_site-03.png', 'cinenova/cine_site-04.png', ]
+    },
+    {
+        index: 6,
+        title_home: 'Entre a Psiquiatria e a Arte (2020)',
+        title_full: 'Entre a Psiquiatria e a Arte (2020)',
+        date: 'October 12, 2019 11:13:00',
+        service: 'Art Direction / Brand Identity / Webdesign',
+        text: '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.<br><br><a class="proj_links" target="blank" href="http://2020.fbaul-dcnm.pt/evolutionary_leap/">Project Website →</a>',
+        collaboration: 'colab w/:<br>Abel Quental<br><br>',
+        thumbnail: 'enip.png',
+        images: ['enip.png']
+    },
+    {
+        index: 7,
+        title_home: 'Proxy, The World (2020)',
+        title_full: 'Proxy, The World (2020)',
+        date: 'May 21, 2020 11:13:00',
+        service: 'Art Direction',
+        text: 'No concelho de Tomar, na freguesia de Madalena, encontramos a aldeia de Cem Soldos, lugar onde, uma vez por ano, a cultura portuguesa é celebrada e partilhada com todos os que a visitam nos dias prometidos. Ao visitar Cem Soldos nos quatro dias eufóricos de verão do festival BONS SONS, a memória que guardei desta aldeia foi a de um movimento permanente, ruas e praças cheias até à claustrofobia, atividade borbulhante que ninguém consegue acompanhar, pontos de cerveja a cada esquina e travessas estreitas utilizadas como vias de passagem de um palco para outro. Sem nunca ter chegado a conhecer este lugar, adivinho que Cem Soldos fica perdido algures no contraste entre a alegria e o desânimo, a agitação e a melancolia, a música e o silêncio. Sem a intervenção do festival transforma-se numa aldeia desconhecida, um espaço lúgubre e tranquilo como todas as outras aldeias.',
+        collaboration: '',
+        thumbnail: 'proxy.svg',
+        images: ['proxy.svg']
+    }, {
+        index: 8,
+        title_home: 'Fantasia (2020)',
+        title_full: 'Fantasia (2020)',
+        date: 'June 01, 2020 11:13:00',
+        service: 'Art Direction / Webdesign / Web Development',
+        text: "No concelho de Tomar, na freguesia de Madalena, encontramos a aldeia de Cem Soldos, lugar onde, uma vez por ano, a cultura portuguesa é celebrada e partilhada com todos os que a visitam nos dias prometidos. Ao visitar Cem Soldos nos quatro dias eufóricos de verão do festival BONS SONS, a memória que guardei desta aldeia foi a de um movimento permanente, ruas e praças cheias até à claustrofobia, atividade borbulhante que ninguém consegue acompanhar, pontos de cerveja a cada esquina e travessas estreitas utilizadas como vias de passagem de um palco para outro. Sem nunca ter chegado a conhecer este lugar, adivinho que Cem Soldos fica perdido algures no contraste entre a alegria e o desânimo, a agitação e a melancolia, a música e o silêncio. Sem a intervenção do festival transforma-se numa aldeia desconhecida, um espaço lúgubre e tranquilo como todas as outras aldeias.<br><br><a class='proj_links' target='blank' href='http://ittakesseveralminutes.belasartes.ulisboa.pt/'>Project Website →</a>",
+        collaboration: '',
+        thumbnail: 'fantasia/fantasia.png',
+        images: ['fantasia/fantasia_flat.png', 'fantasia/borderless.png', 'fantasia/fantasia-logo_1.png', 'fantasia/vynil.png']
+    },
+    {
+        index: 9,
+        title_home: 'Living On The Edge Of (2020)',
+        title_full: 'Living On The Edge Of (2020)',
+        date: 'June 01, 2020 11:13:00',
+        service: 'Art Direction / Webdesign / Web Development',
+        text: "No concelho de Tomar, na freguesia de Madalena, encontramos a aldeia de Cem Soldos, lugar onde, uma vez por ano, a cultura portuguesa é celebrada e partilhada com todos os que a visitam nos dias prometidos. Ao visitar Cem Soldos nos quatro dias eufóricos de verão do festival BONS SONS, a memória que guardei desta aldeia foi a de um movimento permanente, ruas e praças cheias até à claustrofobia, atividade borbulhante que ninguém consegue acompanhar, pontos de cerveja a cada esquina e travessas estreitas utilizadas como vias de passagem de um palco para outro. Sem nunca ter chegado a conhecer este lugar, adivinho que Cem Soldos fica perdido algures no contraste entre a alegria e o desânimo, a agitação e a melancolia, a música e o silêncio. Sem a intervenção do festival transforma-se numa aldeia desconhecida, um espaço lúgubre e tranquilo como todas as outras aldeias.<br><br><a class='proj_links' target='blank' href='http://2020.fbaul-dcnm.pt/'>Project Website →</a>",
+        collaboration: '',
+        thumbnail: 'fantasia/fantasia.png',
+        images: ['edge/edge_logo.svg', 'edge/edge_print-01.png', 'edge/edge_print-02.png']
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var pikus = [
+    '7',
+    "Pikus (2017)",
+    "Pikus (2017)",
+    "pikus.png",
+    '<i>Evolutionary Leap®</i> is a project created with the intent of exploring what it means to be transhuman, what is transhumanism compared with other philosophical ideologies from the same spectrum and to reflect on how transhuman are we already. The goal was to compile a group of “objects”, being them mostly essays, articles, images or projects, that would reflect upon these questions.<br><br>The project is composed by two artifacts: the book and this website. The book is the main archive, containing the majority of the contents, while the website provides a bridge to the transhumanist culture by providing a lexicon explaining common terms associated with the subjects.<br><br><a class="proj_links" target="blank" href="http://2020.fbaul-dcnm.pt/evolutionary_leap/">Project Website →</a>',
+    '',
+    "jtw.png",
+    "jtw.png",
+    "jtw.png",
+    "jtw.png",
+    "jtw.png",
+    "jtw.png",
+];
+projs.push(pikus);*/
