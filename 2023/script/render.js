283c51fe14d3e0bdcbe2ad4165adfd5d081d4f49
diogo1943 <skatelovver@gmail.com>
2022-12-31 18:32:15 +0000

    2023_site

---
+++ 2023/script/render.js
@@ -0,0 +1,30 @@
+var total_layers = 9;
+
+render(total_layers);
+
+
+
+function render(a) {
+
+    var base = document.getElementById('parallax-base');
+
+    for (i = 1; i <= a; i++) {
+
+        var layer = document.createElement('div');
+        layer.setAttribute("id", 'layer' + i)
+        layer.classList.add('layer');
+
+
+        var img = document.createElement('img');
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            img.setAttribute("src", 'layers/2023_0' + i + '-01.png')
+        } else {
+            img.setAttribute("src", 'layers/2023_0' + i + '-01.png')
+        }
+
+        layer.appendChild(img);
+        base.appendChild(layer);
+
+    }
+
+}
